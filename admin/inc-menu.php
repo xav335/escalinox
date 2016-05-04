1a64ea7069bed0aa21e6722f0aa56d2ecfdd7a61
Xavier Gonzalez <xavier@gonzalez.pm>
2016-05-04 17:51:33 +0200

    refacto menu admin

--- admin/inc-menu.php
+++ admin/inc-menu.php
@@ -24,13 +24,6 @@
 					            	<li><a href="/admin/actualite/liste.php">Modif / Suppr</a></li>
 					          	</ul>
 					        </li>
-	                        <li class="dropdown">
-					          	<a href="/admin/livre_dor/liste.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Livre d'or <span class="caret"></span></a>
-					          	<ul class="dropdown-menu" role="menu">
-					            	<li><a href="/admin/livre_dor/edition.php">Ajout</a></li>
-					            	<li><a href="/admin/livre_dor/liste.php">Modif / Suppr</a></li>
-					          	</ul>
-					        </li>
 					         <li class="dropdown">
 					          	<a href="contact-list.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Contacts <span class="caret"></span></a>
 					          	<ul class="dropdown-menu" role="menu">
@@ -40,15 +33,7 @@
 					            	<li><a href="/admin/contact-import.php">Import / Export</a></li>
 					          	</ul>
 					        </li>
-					         <li class="dropdown">
-					          	<a href="newsletter-list.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Newsletter<span class="caret"></span></a>
-					          	<ul class="dropdown-menu" role="menu">
-					            	<li><a href="/admin/newsletter-edit.php">Ajout Newsletter</a></li>
-					            	<li><a href="/admin/newsletter-list.php">Modif / Suppr / Envoi</a></li>
-					            	<li class="divider"></li>
-					            	<li><a href="/admin/newsletterjournal-list.php">Journal des envoi</a></li>
-					          	</ul>
-					        </li>
+					         
 							<!--<li class="dropdown">
 					          	<a href="bdc.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Bon de commande <span class="caret"></span></a>
 					          	<ul class="dropdown-menu" role="menu">
